06274635f93413d4d157189c5d53e3472f2ea3d6
Eduardo Neira <edun327@gmail.com>
2019-11-29 00:31:47 -0300

    Run test sets up environment

---
+++ tests/run_test.py
@@ -0,0 +1,59 @@
+from big_fiubrother_core.message_clients.rabbitmq import Publisher
+from big_fiubrother_core.message import (
+    ProcessedFaceMessage,
+    ProcessedFrameMessage
+)
+from big_fiubrother_core.db import (
+    VideoChunk,
+    Frame,
+    ProcessedFrame,
+    ProcessedVideoChunk,
+    Database
+)
+
+
+db_configuration = {
+    'host': 'localhost',
+    'database': 'big_fiubrother_test',
+    'username': 'postgres',
+    'password': 'password'
+}
+
+db = Database(db_configuration)
+
+publisher_configuration = {
+    'host': 'localhost',
+    'username': 'fiubrother',
+    'password': 'alwayswatching',
+    'exchange': 'fiubrother',
+    'routing_key': 'frames'
+}
+
+publisher = Publisher(publisher_configuration)
+
+video_chunk = VideoChunk(camera_id='CAMERA',
+                         timestamp=0.0,
+                         payload=b'payload')
+
+db.add(video_chunk)
+
+frame_ids = []
+for offset in range(2):
+    frame = Frame(offset=offset,
+                  video_chunk_id=video_chunk.id)
+
+    db.add(frame)
+    frame_ids.append(frame.id)
+
+
+db.add(ProcessedVideoChunk(video_chunk_id=video_chunk.id,
+                           total_frames_count=2))
+
+db.add(ProcessedFrame(frame_id=frame_ids[0],
+                      total_faces_count=2))
+
+
+for frame_id in frame_ids:
+    publisher.publish(ProcessedFaceMessage(frame_id=frame_id))
+
+publisher.publish(ProcessedFrameMessage(video_chunk_id=video_chunk.id))
