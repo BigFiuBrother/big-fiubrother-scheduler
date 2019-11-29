325bc9374a8f55db947eb8c158e10de5ec286775
Eduardo Neira <edun327@gmail.com>
2019-11-29 00:00:12 -0300

    Change to configuration in process_scheduler

--- big_fiubrother_scheduler/process_scheduler.py
+++ big_fiubrother_scheduler/process_scheduler.py
@@ -9,7 +9,7 @@ from big_fiubrother_core.db import (
 class ProcessScheduler:
 
     def __init__(configuration):
-        self.db = Database(configuration['db'])
+        self.db = Database(configuration)
         self.cache = {}
 
     def update_frame_process(self, frame_id):
@@ -17,12 +17,15 @@ class ProcessScheduler:
             self.db.session
             .query()
             .filter(ProcessedFrame.frame_id == frame_id)
-            .update({'processed_faces_count': (ProcessedFrame.processed_faces_count + 1)})
+            .update({
+                'processed_faces_count': (
+                    ProcessedFrame.processed_faces_count + 1)
+            })
 
             processed_frame = self.db.query(ProcessedFrame).get(frame_id)
 
         if processed_frame.is_completed:
-            video_chunk_id = self.db.query(Frame.video_chunk_id).get(frame_id)
+            video_chunk_id = video_chunk_id_for(frame_id)
             return self.update_video_chunk_process(video_chunk_id)
         else:
             return None
@@ -32,12 +35,22 @@ class ProcessScheduler:
             self.db.session
             .query()
             .filter(ProcessedVideoChunk.video_chunk_id == video_chunk_id)
-            .update({'processed_frames_count': (ProcessedVideoChunk.processed_frames_count + 1)})
+            .update({
+                'processed_frames_count': (
+                    ProcessedVideoChunk.processed_frames_count + 1)
+            })
 
-            processed_video_chunk = self.db.query(ProcessedVideoChunk).get(video_chunk_id)
+            processed_video_chunk = self.db.query(
+                ProcessedVideoChunk).get(video_chunk_id)
 
         if processed_video_chunk.is_completed:
             return video_chunk_id
         else:
             return None
 
+    def video_chunk_id_for(frame_id):
+        if frame_id not in self.cache:
+            self.cache[frame_id] = self.db.query(
+                Frame.video_chunk_id).get(frame_id)
+
+        return self.cache[frame_id]
